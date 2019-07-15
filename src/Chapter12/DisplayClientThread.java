5936fb90b1b62ff36222dcb66ddc58347c29d5f1
Yuji Uchiyama <y.uchiyama.1015@gmail.com>
2019-07-15 13:28:10 +0900

    利用側のクラスを実装

--- src/Chapter12/DisplayClientThread.java
+++ src/Chapter12/DisplayClientThread.java
@@ -1,12 +1,22 @@
 import activeobject.ActiveObject;
-// import activeobject.Result;
 
 public class DisplayClientThread extends Thread {
 
+    private final ActiveObject activeObject;
+
     public DisplayClientThread(String name, ActiveObject activeObject) {
 	super(name);
+	this.activeObject = activeObject;
     }
 
     public void run() {
+	try {
+	    for (int i = 0; true; i++) {
+		String string = Thread.currentThread().getName() + " " + i;
+		activeObject.displayString(string);
+		Thread.sleep(200);
+	    }
+	} catch (InterruptedException e) {
+	}
     }
 }
--- src/Chapter12/activeobject/ActiveObject.java
+++ src/Chapter12/activeobject/ActiveObject.java
@@ -1,4 +1,8 @@
 package activeobject;
 
 public interface ActiveObject {
+
+    public abstract Result<String> makeString(int i, char fillchar);
+
+    public abstract void displayString(String string);
 }
