215d910cdff20115867f0b6041258aec4a76ab70
Yuji Uchiyama <y.uchiyama.1015@gmail.com>
2019-07-15 14:20:09 +0900

    Result クラス群を実装

--- src/Chapter12/activeobject/FutureResult.java
+++ src/Chapter12/activeobject/FutureResult.java
@@ -2,10 +2,27 @@ package activeobject;
 
 class FutureResult<T> extends Result<T> {
 
-    public T getResultValue() {
-	return null;
+    private Result<T> result;
+    private boolean ready;
+
+    public FutureResult() {
+	this.result = null;
+	this.ready = false;
     }
 
-    public void setResult(Result<T> result) {
+    public synchronized void setResult(Result<T> result) {
+	this.result = result;
+	this.ready = true;
+	notifyAll();
+    }
+
+    public synchronized T getResultValue() {
+	while (!ready) {
+	    try {
+		wait();
+	    } catch (InterruptedException e) {
+	    }
+	}
+	return result.getResultValue();
     }
 }
